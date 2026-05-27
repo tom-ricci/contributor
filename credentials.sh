1ef4a9c79d2ed6984128e8018a428bae65053cd1
Thomas Ricci <me@thomasricci.dev>
2026-05-26 22:39:01 -0400

    Update credentials.sh

--- credentials.sh
+++ credentials.sh
@@ -1,4 +1,4 @@
-# This file contains commands to set your Git email and name. Contributor will use these credentials to commit your commits from your sources to your copy of the Contributor repository. This script will be executed right before Contributor commits. In theory you could write other code here to run inside the Contributor workflow, but I'd recommend editing the workflow file directly.
+# This file contains commands to set your Git email and name.
 
 git config --global user.email "yourname@example.com"
 git config --global user.name "yourname"
